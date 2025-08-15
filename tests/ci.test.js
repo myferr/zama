6a680aa3dcf9af05fccfd4ba8f61454abacc61a7
myferr <contactme.myfer@protonmail.com>
2025-08-15 18:26:53 -0500

    chore: add biome lint workflow and update CI test

--- tests/ci.test.js
+++ tests/ci.test.js
@@ -7,6 +7,7 @@ import chalk from "chalk";
 console.log(`${chalk.blue("[zama](process)")} Testing CI with act...`);
 try {
   execSync("act -W .github/workflows/build.yml", { stdio: "inherit" });
+  execSync("act -W .github/workflows/biome.yml", { stdio: "inherit" });
   console.log(`${chalk.green("[zama](success)")} Complete!`);
 } catch (_error) {
   console.log(
